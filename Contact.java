f16a57e82c9c4e6b2c21977cff9e6b8e1a1ef2a4
yahknoe <yahknoe@gmail.com>
2013-03-14 14:32:37 -0700

    Updated Contact.java
    Contact class should now be fully functional

--- Contact.java
+++ Contact.java
@@ -25,17 +25,18 @@ public class Contact { // KZ.
 	 * "uses printSingleContact to output to console"
 	 * "   ~METHOD sets whole contact at once"
 	 */
-	public void createContact () {	 // KZ. 
-		System.out.println("METHOD createContact scans/reads input from user and create a single contact");
-		setLastName("last");
-		setFirstName("first");
-		setStreet("street");
-		setCity("city");
-		setState("state");
-		setZip("zip");
-		setEmail("email");
-		setPhone("phone");
-		setNotes("notes");
+	public Contact readContact() {	 // KZ. 
+		Contact thisContact = new Contact();
+		thisContact.setLastName(getLastName());
+		thisContact.setFirstName(getFirstName());
+		thisContact.setStreet(getStreet());
+		thisContact.setCity(getCity());
+		thisContact.setState(getState());
+		thisContact.setZip(getZip());
+		thisContact.setEmail(getEmail());
+		thisContact.setPhone(getPhone());
+		thisContact.setNotes(getNotes());
+		return thisContact;
 	}
 
 	/*
@@ -54,16 +55,17 @@ public class Contact { // KZ.
 	 * "      ~ Method setLastName: Set lastName"
 	 */
 	public void setLastName(String lN) { // KZ, EM
-		System.out.println("      ~ Method setLastName: Set lastName");
 		lastName = lN;
 	}
-	
+
 	/*
 	 * Method to get last name
 	 * "      ~ Method getLastName: Get lastName"
 	 */
 	public String getLastName() { // EM, KZ
-		System.out.println("      ~ Method getLastName: Get lastName");
+		Scanner reader = new Scanner(System.in);
+		System.out.print("Last Name: ");
+		lastName = reader.nextLine();
 		return lastName;
 	}
 
@@ -72,106 +74,112 @@ public class Contact { // KZ.
 	 * "      ~ Method setFirstName: Set firstName"
 	 */
 	public void setFirstName(String fN) { // KZ, EM
-		System.out.println("      ~ Method setFirstName: Set firstName");
 		firstName = fN;
 	}
-	
+
 	/*
 	 * Method to get first name
 	 * "      ~ Method getFirstName: Get firstName"
 	 */
 	public String getFirstName() { // EM, KZ
-		System.out.println("      ~ Method getFirstName: Get firstName");
+		Scanner reader = new Scanner(System.in);
+		System.out.print("First Name: ");
+		firstName = reader.nextLine();
 		return firstName;
 	}
-	
+
 	/*
 	 * Method to set street address
 	 * "      ~ Method setStreet: Set street"
 	 */
 	public void setStreet(String s) { // KZ, EM
-		System.out.println("      ~ Method setStreet: Set street");
 		street = s;
 	}
-	
+
 	/*
 	 * Method to get street address
 	 * "      ~ Method getStreet: Get street"
 	 */
 	public String getStreet() { // EM, KZ
-		System.out.println("      ~ Method getStreet: Get street");
+		Scanner reader = new Scanner(System.in);
+		System.out.print("Street Address: ");
+		street = reader.nextLine();
 		return street;
 	}
-	
+
 	/*
 	 * Method to set city address
 	 * "      ~ Method setCity: Set city"
 	 */
 	public void setCity(String c) { // KZ, EM
-		System.out.println("      ~ Method setCity: Set city");
 		city = c;
 	}
-	
+
 	/*
 	 * Method to get city address
 	 * "      ~ Method getCity: Get city"
 	 */
 	public String getCity() { // EM, KZ
-		System.out.println("      ~ Method getCity: Get city");
+		Scanner reader = new Scanner(System.in);
+		System.out.print("City: ");
+		city = reader.nextLine();
 		return city;
 	}
-	
+
 	/*
 	 * Method to set state
 	 * "      ~ Method setState: Set state"
 	 */
 	public void setState(String s) { // KZ, EM
-		System.out.println("      ~ Method setState: Set state");
 		state = s;
 	}
-	
+
 	/*
 	 * Method to get state
 	 * "      ~ Method getState: Get state"
 	 */
 	public String getState() { // EM, KZ
-		System.out.println("      ~ Method getState: Get state");
+		Scanner reader = new Scanner(System.in);
+		System.out.print("State: ");
+		state = reader.nextLine();
 		return state;
 	}
-	
+
 	/*
 	 * Method to set zip code
 	 * "      ~ Method setZip: Set zip"
 	 */
 	public void setZip(String z) { // KZ, EM
-		System.out.println("      ~ Method setZip: Set zip");
 		zip = z;
 	}
-	
+
 	/*
 	 * Method to get zip code
 	 * "      ~ Method getZip: Get zip"
 	 */
 	public String getZip() { // EM, KZ
-		System.out.println("      ~ Method getZip: Get zip");
+		Scanner reader = new Scanner(System.in);
+		System.out.print("Zip: ");
+		zip = reader.nextLine();
 		return zip;
 	}
-	
+
 	/*
 	 * Method to set email address
 	 * "      ~ Method setEmail: Set email"
 	 */
 	public void setEmail(String e) { // KZ, EM
-		System.out.println("      ~ Method setEmail: Set email");
 		email = e;
 	}
-	
+
 	/*
 	 * Method to get email address
 	 * "      ~ Method getEmail: Get email"
 	 */
 	public String getEmail() { // EM, KZ
-		System.out.println("      ~ Method getEmail: Get email");
+		Scanner reader = new Scanner(System.in);
+		System.out.print("Email Address: ");
+		email = reader.nextLine();
 		return email;
 	}
 
@@ -180,16 +188,17 @@ public class Contact { // KZ.
 	 * "      ~ Method setPhone: Set phone"
 	 */
 	public void setPhone(String p) { // KZ, EM
-		System.out.println("      ~ Method setPhone: Set phone");
 		phone = p;
 	}
-	
+
 	/*
 	 * Method to get phone number
 	 * "      ~ Method getPhone: Get phone"
 	 */
 	public String getPhone() { // EM, KZ
-		System.out.println("      ~ Method getPhone: Get phone");
+		Scanner reader = new Scanner(System.in);
+		System.out.print("Phone Number: ");
+		phone = reader.nextLine();
 		return phone;
 	}
 	/*
@@ -197,26 +206,28 @@ public class Contact { // KZ.
 	 * "      ~ Method setNotes: Set notes"
 	 */
 	public void setNotes(String n) { // KZ, EM
-		System.out.println("      ~ Method setNotes: Set notes");
 		notes = n;
 	}
-	
+
 	/*
 	 * Method to set notes
 	 * "      ~ Method getNotes: Get notes"
 	 */
 	public String getNotes() { // EM, KZ
-		System.out.println("      ~ Method getNotes: Get notes");
+		Scanner reader = new Scanner(System.in);
+		System.out.print("Notes: ");
+		notes = reader.nextLine();
 		return notes;
 	}
-	
+
 	/*
      * Getter method for key by which to sort Contact objects EM
      */
     public String getKey() {
     	key = lastName + ", " + firstName;
         return key;
-    }
+   	}
+   	
 	// TEMP TEMP TEMP TEMP TEMP TEMP TEMP TEMP TEMP TEMP TEMP TEMP TEMP TEMP TEMP TEMP TEMP TEMP 
 	public void readContact() { // KZ. TEMPORARY to FIX ERROR IN CONTACT CALL
 		System.out.println("TEMP METHOD readContact");
