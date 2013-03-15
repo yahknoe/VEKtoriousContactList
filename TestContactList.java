b682d26b9948f3ac53d0a3ba3d30ba52a3116a7b
krysiak2000 <krysia.k2000@gmail.com>
2013-03-14 21:28:14 -0700

    Update TestContactList.java

--- TestContactList.java
+++ TestContactList.java
@@ -14,26 +14,38 @@ public class TestContactList { // KZ.
 		demoContactList = new ContactList(); // EM
 		Contact demoContact; // EM
 		demoContact = new Contact(); // EM
+		
+		System.out.println(" •• 01 - TestContactList: print demoContact");
+		System.out.println("    demoContact: " + demoContact);
 
-		// read contacts from disk to start where we left off
+		// read all contacts from disk
+		System.out.println(" •• 02 - TestContactList: demoContactList.readFile()");
 		demoContactList.readFile(); // KZ. read file from disk
 
 		// loop to be implemented to repeat menu until user quits
+		System.out.println(" •• 03 - TestContactList: demoContactList.menu()");
 		demoContactList.menu(); // KZ. call main menu, displays menu options
 
 		// MENU OPTIONS -- For demonstration purposes only
-		demoContact.readContact(); // EM
-		System.out.println(demoContact.printSingleContact()); // EM
+			System.out.println(" •• 04 - TestContactList: REMOVED •• demoContact.readContact()");
+			// demoContact.readContact(); // EM (REMOVED AS THIS CAUSES A NEW CONTACT AFTER MENU QUIT
+			System.out.println(" •• 05 - TestContactList: demoContact.printSingleContact()");
+			System.out.println(demoContact.printSingleContact()); // EM
 
+		System.out.println(" •• 06 - TestContactList: demoContactList.printList()");
 		demoContactList.printList(); // KZ.
+		System.out.println(" •• 07 - TestContactList: demoContactList.retrieveLastName()");
 		demoContactList.retrieveLastName(); // KZ.
+		System.out.println(" •• 08 -TestContactList: demoContactList.retrieveEmail()");
 		demoContactList.retrieveEmail(); // KZ.
+		System.out.println(" •• 09 -TestContactList: demoContactList.retrieveZip()");
 		demoContactList.retrieveZip(); // KZ.
 
 		// end of loop means user quit; save to disk
+		System.out.println(" •• 10 -TestContactList: demoContactList.writeFile()");
 		demoContactList.writeFile(); // KZ. write changes to disk
-	
-	
+
+
 		//USE THIS TO TEST ADDING CONTACTS TO LIST AND PRINTING:
 		/*
 		demoContactList.add(demoContact.readContact()); // EM
@@ -45,39 +57,160 @@ public class TestContactList { // KZ.
 		demoContactList.add(demoContact.readContact()); // EM
 		demoContactList.printList(); // EM
 		*/
-	
-	
-	
+
+
+
 	}
 }
 
 /*
 OUR OUTPUT:
 
-METHOD readFile: Create ArrayList w/ ArrayList
-METHOD menu: Output Menu and scan for Menu Options
-METHOD readContact scans/reads input from user and create a single contact
-      ~ Method setLastName: Set lastName
-      ~ Method setFirstName: Set firstName
-      ~ Method setStreet: Set street
-      ~ Method setCity: Set city
-      ~ Method setState: Set state
-      ~ Method setZip: Set zip
-      ~ Method setEmail: Set email
-      ~ Method setPhone: Set phone
-      ~ Method setNotes: Set notes
-METHOD printSingleContact: Print out single complete contact
-last, first
-street
-city, state zip
-email
-phone
-notes
-METHOD newContact: Call contact and create new/single contact
-METHOD printList: Print sorted list to console.
-METHOD retrieveLastName: Retrieve contact(s) with matching last name & print to console or return error if none match
-METHOD retrieveEmail: Retrieve contact(s) with matching email & print to console or return error if none match
-METHOD retrieveZip: Retrieve contact(s) with matching zip & print to console or return error if none match
-METHOD writeFile: Write data from file into ArrayList
+ •• 01 - TestContactList: print demoContact
+    demoContact: Contact@4b71bbc9
+ •• 02 - TestContactList: demoContactList.readFile()
+ -- ContactList: METHOD readFile: Read Contact objects from saved file 
+    and add to contactArrayList()
+ •• 03 - TestContactList: demoContactList.menu()
+--------------------------------------------------------
+--- Welcome to the Quality Soft Contact List Program ---
+--------------------------------------------------------
+--------------------   MAIN MENU:   --------------------
+--------------------------------------------------------
+        (1) New contact
+        (2) Print contact list
+        (3) Retrieve contact(s) by last name
+        (4) Retrieve contact(s) by email address
+        (5) Retrieve contact(s) by zip code
+        (6) Quit Contact List Program
+--------------------------------------------------------
+  What would you like to do? (Enter option from above):
+1
+switch menuChoice: new
+Last Name: 
+--- You did not enter a last name. 
+--- Please enter a valid last name to start a new contact.
+Last Name: Johnson
+First Name: Kim
+Street Address: 345 Main St
+City: Somplace
+State: NO
+Zip: 03030
+Email Address: kasjdu@wherever.com
+Phone Number: 555-555-5555
+Notes: what ever will be will be
+--------------------------------------------------------
+--------------------   MAIN MENU:   --------------------
+--------------------------------------------------------
+        (1) New contact
+        (2) Print contact list
+        (3) Retrieve contact(s) by last name
+        (4) Retrieve contact(s) by email address
+        (5) Retrieve contact(s) by zip code
+        (6) Quit Contact List Program
+--------------------------------------------------------
+  What would you like to do? (Enter option from above):
+2
+switch menuChoice: print
+ - @ - Class Conact: METHOD printSingleContact: Print out complete contact
+Contact: Johnson, Kim
+         345 Main St
+         Somplace, NO 03030
+         kasjdu@wherever.com
+         555-555-5555
+         what ever will be will be
+--------------------------------------------------------
+--------------------   MAIN MENU:   --------------------
+--------------------------------------------------------
+        (1) New contact
+        (2) Print contact list
+        (3) Retrieve contact(s) by last name
+        (4) Retrieve contact(s) by email address
+        (5) Retrieve contact(s) by zip code
+        (6) Quit Contact List Program
+--------------------------------------------------------
+  What would you like to do? (Enter option from above):
+3
+switch menuChoice: last
+ -- ContactList: METHOD retrieveLastName: Retrieve contact(s) with 
+    matching last name & print to console or return error if none match
+ -- ContactList: METHOD sortContact: Sort contact list in alphabetical order.
+--------------------------------------------------------
+--------------------   MAIN MENU:   --------------------
+--------------------------------------------------------
+        (1) New contact
+        (2) Print contact list
+        (3) Retrieve contact(s) by last name
+        (4) Retrieve contact(s) by email address
+        (5) Retrieve contact(s) by zip code
+        (6) Quit Contact List Program
+--------------------------------------------------------
+  What would you like to do? (Enter option from above):
+4
+switch menuChoice: email
+ -- ContactList: METHOD retrieveEmail: Retrieve contact(s) with 
+    matching email & print to console or return error if none match
+ -- ContactList: METHOD sortContact: Sort contact list in alphabetical order.
+--------------------------------------------------------
+--------------------   MAIN MENU:   --------------------
+--------------------------------------------------------
+        (1) New contact
+        (2) Print contact list
+        (3) Retrieve contact(s) by last name
+        (4) Retrieve contact(s) by email address
+        (5) Retrieve contact(s) by zip code
+        (6) Quit Contact List Program
+--------------------------------------------------------
+  What would you like to do? (Enter option from above):
+5
+switch menuChoice: zip
+ -- ContactList: METHOD retrieveZip: Retrieve contact(s) with 
+    matching zip & print to console or return error if none match
+ -- ContactList: METHOD sortContact: Sort contact list in alphabetical order.
+--------------------------------------------------------
+--------------------   MAIN MENU:   --------------------
+--------------------------------------------------------
+        (1) New contact
+        (2) Print contact list
+        (3) Retrieve contact(s) by last name
+        (4) Retrieve contact(s) by email address
+        (5) Retrieve contact(s) by zip code
+        (6) Quit Contact List Program
+--------------------------------------------------------
+  What would you like to do? (Enter option from above):
+6
+switch menuChoice: quit
+Thank you for using the contact list!
+ -- ContactList: METHOD writeFile: Write Contact object data from 
+    ArrayList to file
+ •• 04 - TestContactList: REMOVED •• demoContact.readContact()
+ •• 05 - TestContactList: demoContact.printSingleContact()
+ - @ - Class Conact: METHOD printSingleContact: Print out complete contact
+Contact: null, null
+         null
+         null, null null
+         null
+         null
+         null
+ •• 06 - TestContactList: demoContactList.printList()
+ - @ - Class Conact: METHOD printSingleContact: Print out complete contact
+Contact: Johnson, Kim
+         345 Main St
+         Somplace, NO 03030
+         kasjdu@wherever.com
+         555-555-5555
+         what ever will be will be
+ •• 07 - TestContactList: demoContactList.retrieveLastName()
+ -- ContactList: METHOD retrieveLastName: Retrieve contact(s) with 
+    matching last name & print to console or return error if none match
+ •• 08 -TestContactList: demoContactList.retrieveEmail()
+ -- ContactList: METHOD retrieveEmail: Retrieve contact(s) with 
+    matching email & print to console or return error if none match
+ •• 09 -TestContactList: demoContactList.retrieveZip()
+ -- ContactList: METHOD retrieveZip: Retrieve contact(s) with 
+    matching zip & print to console or return error if none match
+ •• 10 -TestContactList: demoContactList.writeFile()
+ -- ContactList: METHOD writeFile: Write Contact object data from 
+    ArrayList to file
 
 */
