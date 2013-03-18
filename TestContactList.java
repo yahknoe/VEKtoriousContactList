1bc9c7e56c4fc4bca3cb9e66d94f27bdedac7969
emarinelli <elena.marinelli@gmail.com>
2013-03-17 22:18:24 -0700

    New test run output; commented out demo retrieve()
    Performed a test run showcasing new and established features and added output to "Output to Console." Commented out retrieveLastName(), retrieveEmail(), and retrieveZip() methods from menu options demo as these functioning methods will now request user input for a search rather than quitting.

--- TestContactList.java
+++ TestContactList.java
@@ -27,18 +27,18 @@ public class TestContactList { // KZ.
 		demoContactList.menu(); // KZ. call main menu, displays menu options
 
 		// MENU OPTIONS -- For demonstration purposes only
-		System.out.println(" ** 04 - TestContactList: REMOVED •• demoContact.readContact()");
-		// demoContact.readContact(); // EM (REMOVED AS THIS CAUSES A NEW CONTACT AFTER MENU QUIT
+		System.out.println(" ** 04 - TestContactList: REMOVED ‚Ä¢‚Ä¢¬†demoContact.readContact()");
+		// demoContact.readContact(); // EM (REMOVED AS THIS CAUSES A NEW CONTACT AFTER MENU QUIT)
 		System.out.println(" ** 05 - TestContactList: demoContact.returnSingleContact()");
 		System.out.println(demoContact.returnSingleContact()); // EM
 		System.out.println(" ** 06 - TestContactList: demoContactList.printList()");
 		demoContactList.printList(); // KZ.
 		System.out.println(" ** 07 - TestContactList: demoContactList.retrieveLastName()");
-		demoContactList.retrieveLastName(); // KZ.
+		// demoContactList.retrieveLastName(); // KZ. (REMOVED AS THIS CAUSES A NEW CONTACT AFTER MENU QUIT)
 		System.out.println(" ** 08 -TestContactList: demoContactList.retrieveEmail()");
-		demoContactList.retrieveEmail(); // KZ.
+		// demoContactList.retrieveEmail(); // KZ. (REMOVED AS THIS CAUSES A NEW CONTACT AFTER MENU QUIT)
 		System.out.println(" ** 09 -TestContactList: demoContactList.retrieveZip()");
-		demoContactList.retrieveZip(); // KZ.
+		// demoContactList.retrieveZip(); // KZ. (REMOVED AS THIS CAUSES A NEW CONTACT AFTER MENU QUIT)
 
 		// end of loop means user quit; save to disk
 		System.out.println(" ** 10 -TestContactList: demoContactList.writeFile()");
@@ -50,9 +50,8 @@ public class TestContactList { // KZ.
 --------------------------------------------------------
 ----------------   OUTPUT TO CONSOLE:   ----------------
 --------------------------------------------------------
-
  ** 01 - TestContactList: print demoContact
-    demoContact: Contact@3487a5cc
+    demoContact: Contact@f7e6a96
  ** 02 - TestContactList: demoContactList.readFile()
  -- ContactList: METHOD readFile: Read Contact objects from saved file 
     and add to contactArrayList()
@@ -97,7 +96,7 @@ Email Address: VEK@win.us
     or   your.url_02@somplace.xxx 
 
 Email Address: VEK@win.usa
-Phone Number: 1234567890
+Phone Number: yay
 --- You did not enter a valid US phone number. 
 --- Please enter a phone number in the following format: XXX-XXX-XXXX.
 Phone Number: 123-456-7890
@@ -116,7 +115,7 @@ Notes: Lookin' good, feelin' fine.
 switch menuChoice: new
 Last Name: Sort
 First Name: AlphabetInTheRoom
-Street Address: 456 Zoo Ln
+Street Address: 456 Zoo Ln.
 City: Tuscany
 State: :)
 Zip: 98765
@@ -133,43 +132,17 @@ Notes: Notable, but only just.
         (5) Retrieve contact(s) by zip code
         (6) Quit Contact List Program
 --------------------------------------------------------
-  What would you like to do? (Enter option from above): 2
-switch menuChoice: print
- - @ - Class Contact: METHOD printSingleContact: Print out complete contact
-Contact: Llo, He
-         123 Yay Dr.
-         Yippee, HA 12345
-         VEK@win.usa
-         123-456-7890
-         Lookin' good, feelin' fine.
- - @ - Class Contact: METHOD printSingleContact: Print out complete contact
-Contact: Sort, AlphabetInTheRoom
-         456 Zoo Ln
-         Tuscany, :) 98765
-         list@sorted.yup
-         987-654-3210
-         Notable, but only just.
---------------------------------------------------------
---------------------   MAIN MENU:   --------------------
---------------------------------------------------------
-        (1) New contact
-        (2) Print contact list
-        (3) Retrieve contact(s) by last name
-        (4) Retrieve contact(s) by email address
-        (5) Retrieve contact(s) by zip code
-        (6) Quit Contact List Program
---------------------------------------------------------
   What would you like to do? (Enter option from above): 1
 switch menuChoice: new
-Last Name: Atlas
-First Name: Any
-Street Address: 112 3rd Ave
-City: Anytown
-State: DK
-Zip: 23568
-Email Address: first@list.wrk
-Phone Number: 111-111-1111
-Notes: 'A' is for 'ahead of the rest.'
+Last Name: Sort
+First Name: Yourself
+Street Address: 654 Aquarium Ave.
+City: Tuscany
+State: :)
+Zip: 98765
+Email Address: list@sorted.yup
+Phone Number: 246-801-3579
+Notes: Parents decided not to name "AlphabetInTheRoom Jr."
 --------------------------------------------------------
 --------------------   MAIN MENU:   --------------------
 --------------------------------------------------------
@@ -183,13 +156,6 @@ Notes: 'A' is for 'ahead of the rest.'
   What would you like to do? (Enter option from above): 2
 switch menuChoice: print
  - @ - Class Contact: METHOD printSingleContact: Print out complete contact
-Contact: Atlas, Any
-         112 3rd Ave
-         Anytown, DK 23568
-         first@list.wrk
-         111-111-1111
-         'A' is for 'ahead of the rest.'
- - @ - Class Contact: METHOD printSingleContact: Print out complete contact
 Contact: Llo, He
          123 Yay Dr.
          Yippee, HA 12345
@@ -198,11 +164,18 @@ Contact: Llo, He
          Lookin' good, feelin' fine.
  - @ - Class Contact: METHOD printSingleContact: Print out complete contact
 Contact: Sort, AlphabetInTheRoom
-         456 Zoo Ln
+         456 Zoo Ln.
          Tuscany, :) 98765
          list@sorted.yup
          987-654-3210
          Notable, but only just.
+ - @ - Class Contact: METHOD printSingleContact: Print out complete contact
+Contact: Sort, Yourself
+         654 Aquarium Ave.
+         Tuscany, :) 98765
+         list@sorted.yup
+         246-801-3579
+         Parents decided not to name "AlphabetInTheRoom Jr."
 --------------------------------------------------------
 --------------------   MAIN MENU:   --------------------
 --------------------------------------------------------
@@ -215,8 +188,67 @@ Contact: Sort, AlphabetInTheRoom
 --------------------------------------------------------
   What would you like to do? (Enter option from above): 3
 switch menuChoice: last
- -- ContactList: METHOD retrieveLastName: Retrieve contact(s) with 
-    matching last name & print to console or return error if none match
+***In method retrieveLastName()***
+Last name to search for: Llo
+Searching for last name Llo...
+
+Match found for Llo at index 0!
+ - @ - Class Contact: METHOD printSingleContact: Print out complete contact
+Contact: Llo, He
+         123 Yay Dr.
+         Yippee, HA 12345
+         VEK@win.usa
+         123-456-7890
+         Lookin' good, feelin' fine.
+--------------------------------------------------------
+--------------------   MAIN MENU:   --------------------
+--------------------------------------------------------
+        (1) New contact
+        (2) Print contact list
+        (3) Retrieve contact(s) by last name
+        (4) Retrieve contact(s) by email address
+        (5) Retrieve contact(s) by zip code
+        (6) Quit Contact List Program
+--------------------------------------------------------
+  What would you like to do? (Enter option from above): 3
+switch menuChoice: last
+***In method retrieveLastName()***
+Last name to search for: Sort
+Searching for last name Sort...
+
+Match found for Sort at index 1!
+ - @ - Class Contact: METHOD printSingleContact: Print out complete contact
+Contact: Sort, AlphabetInTheRoom
+         456 Zoo Ln.
+         Tuscany, :) 98765
+         list@sorted.yup
+         987-654-3210
+         Notable, but only just.
+
+Match found for Sort at index 2!
+ - @ - Class Contact: METHOD printSingleContact: Print out complete contact
+Contact: Sort, Yourself
+         654 Aquarium Ave.
+         Tuscany, :) 98765
+         list@sorted.yup
+         246-801-3579
+         Parents decided not to name "AlphabetInTheRoom Jr."
+--------------------------------------------------------
+--------------------   MAIN MENU:   --------------------
+--------------------------------------------------------
+        (1) New contact
+        (2) Print contact list
+        (3) Retrieve contact(s) by last name
+        (4) Retrieve contact(s) by email address
+        (5) Retrieve contact(s) by zip code
+        (6) Quit Contact List Program
+--------------------------------------------------------
+  What would you like to do? (Enter option from above): 3
+switch menuChoice: last
+***In method retrieveLastName()***
+Last name to search for: NotHere
+Searching for last name NotHere...
+Sorry, there are no contacts with last name NotHere in the Contact List.
 --------------------------------------------------------
 --------------------   MAIN MENU:   --------------------
 --------------------------------------------------------
@@ -229,8 +261,67 @@ switch menuChoice: last
 --------------------------------------------------------
   What would you like to do? (Enter option from above): 4
 switch menuChoice: email
- -- ContactList: METHOD retrieveEmail: Retrieve contact(s) with 
-    matching email & print to console or return error if none match
+***In method retrieveEmail()***
+Email to search for: VEK@win.usa
+Searching for email VEK@win.usa...
+
+Match found for VEK@win.usa at index 0!
+ - @ - Class Contact: METHOD printSingleContact: Print out complete contact
+Contact: Llo, He
+         123 Yay Dr.
+         Yippee, HA 12345
+         VEK@win.usa
+         123-456-7890
+         Lookin' good, feelin' fine.
+--------------------------------------------------------
+--------------------   MAIN MENU:   --------------------
+--------------------------------------------------------
+        (1) New contact
+        (2) Print contact list
+        (3) Retrieve contact(s) by last name
+        (4) Retrieve contact(s) by email address
+        (5) Retrieve contact(s) by zip code
+        (6) Quit Contact List Program
+--------------------------------------------------------
+  What would you like to do? (Enter option from above): 4
+switch menuChoice: email
+***In method retrieveEmail()***
+Email to search for: list@sorted.yup
+Searching for email list@sorted.yup...
+
+Match found for list@sorted.yup at index 1!
+ - @ - Class Contact: METHOD printSingleContact: Print out complete contact
+Contact: Sort, AlphabetInTheRoom
+         456 Zoo Ln.
+         Tuscany, :) 98765
+         list@sorted.yup
+         987-654-3210
+         Notable, but only just.
+
+Match found for list@sorted.yup at index 2!
+ - @ - Class Contact: METHOD printSingleContact: Print out complete contact
+Contact: Sort, Yourself
+         654 Aquarium Ave.
+         Tuscany, :) 98765
+         list@sorted.yup
+         246-801-3579
+         Parents decided not to name "AlphabetInTheRoom Jr."
+--------------------------------------------------------
+--------------------   MAIN MENU:   --------------------
+--------------------------------------------------------
+        (1) New contact
+        (2) Print contact list
+        (3) Retrieve contact(s) by last name
+        (4) Retrieve contact(s) by email address
+        (5) Retrieve contact(s) by zip code
+        (6) Quit Contact List Program
+--------------------------------------------------------
+  What would you like to do? (Enter option from above): 4
+switch menuChoice: email
+***In method retrieveEmail()***
+Email to search for: nothere@here.not
+Searching for email nothere@here.not...
+Sorry, there are no contacts with email nothere@here.not in the Contact List.
 --------------------------------------------------------
 --------------------   MAIN MENU:   --------------------
 --------------------------------------------------------
@@ -243,8 +334,69 @@ switch menuChoice: email
 --------------------------------------------------------
   What would you like to do? (Enter option from above): 5
 switch menuChoice: zip
- -- ContactList: METHOD retrieveZip: Retrieve contact(s) with 
-    matching zip & print to console or return error if none match
+***In method retrieveZip()***
+Zip code to search for: 12345
+Searching for zip code 12345...
+
+Match found for 12345 at index 0!
+ - @ - Class Contact: METHOD printSingleContact: Print out complete contact
+Contact: Llo, He
+         123 Yay Dr.
+         Yippee, HA 12345
+         VEK@win.usa
+         123-456-7890
+         Lookin' good, feelin' fine.
+--------------------------------------------------------
+--------------------   MAIN MENU:   --------------------
+--------------------------------------------------------
+        (1) New contact
+        (2) Print contact list
+        (3) Retrieve contact(s) by last name
+        (4) Retrieve contact(s) by email address
+        (5) Retrieve contact(s) by zip code
+        (6) Quit Contact List Program
+--------------------------------------------------------
+  What would you like to do? (Enter option from above): 98765
+You have entered an invalid choice. Please re-enter your menu choice
+5
+switch menuChoice: zip
+***In method retrieveZip()***
+Zip code to search for: 98765
+Searching for zip code 98765...
+
+Match found for 98765 at index 1!
+ - @ - Class Contact: METHOD printSingleContact: Print out complete contact
+Contact: Sort, AlphabetInTheRoom
+         456 Zoo Ln.
+         Tuscany, :) 98765
+         list@sorted.yup
+         987-654-3210
+         Notable, but only just.
+
+Match found for 98765 at index 2!
+ - @ - Class Contact: METHOD printSingleContact: Print out complete contact
+Contact: Sort, Yourself
+         654 Aquarium Ave.
+         Tuscany, :) 98765
+         list@sorted.yup
+         246-801-3579
+         Parents decided not to name "AlphabetInTheRoom Jr."
+--------------------------------------------------------
+--------------------   MAIN MENU:   --------------------
+--------------------------------------------------------
+        (1) New contact
+        (2) Print contact list
+        (3) Retrieve contact(s) by last name
+        (4) Retrieve contact(s) by email address
+        (5) Retrieve contact(s) by zip code
+        (6) Quit Contact List Program
+--------------------------------------------------------
+  What would you like to do? (Enter option from above): 5
+switch menuChoice: zip
+***In method retrieveZip()***
+Zip code to search for: 99999
+Searching for zip code 99999...
+Sorry, there are no contacts with zip code 99999 in the Contact List.
 --------------------------------------------------------
 --------------------   MAIN MENU:   --------------------
 --------------------------------------------------------
@@ -260,7 +412,7 @@ switch menuChoice: quit
 Thank you for using the contact list!
  -- ContactList: METHOD writeFile: Write Contact object data from 
     ArrayList to file
- ** 04 - TestContactList: REMOVED •• demoContact.readContact()
+ ** 04 - TestContactList: REMOVED ‚Ä¢‚Ä¢¬†demoContact.readContact()
  ** 05 - TestContactList: demoContact.returnSingleContact()
  - @ - Class Contact: METHOD printSingleContact: Print out complete contact
 Contact: null, null
@@ -271,13 +423,6 @@ Contact: null, null
          null
  ** 06 - TestContactList: demoContactList.printList()
  - @ - Class Contact: METHOD printSingleContact: Print out complete contact
-Contact: Atlas, Any
-         112 3rd Ave
-         Anytown, DK 23568
-         first@list.wrk
-         111-111-1111
-         'A' is for 'ahead of the rest.'
- - @ - Class Contact: METHOD printSingleContact: Print out complete contact
 Contact: Llo, He
          123 Yay Dr.
          Yippee, HA 12345
@@ -286,22 +431,22 @@ Contact: Llo, He
          Lookin' good, feelin' fine.
  - @ - Class Contact: METHOD printSingleContact: Print out complete contact
 Contact: Sort, AlphabetInTheRoom
-         456 Zoo Ln
+         456 Zoo Ln.
          Tuscany, :) 98765
          list@sorted.yup
          987-654-3210
          Notable, but only just.
+ - @ - Class Contact: METHOD printSingleContact: Print out complete contact
+Contact: Sort, Yourself
+         654 Aquarium Ave.
+         Tuscany, :) 98765
+         list@sorted.yup
+         246-801-3579
+         Parents decided not to name "AlphabetInTheRoom Jr."
  ** 07 - TestContactList: demoContactList.retrieveLastName()
- -- ContactList: METHOD retrieveLastName: Retrieve contact(s) with 
-    matching last name & print to console or return error if none match
  ** 08 -TestContactList: demoContactList.retrieveEmail()
- -- ContactList: METHOD retrieveEmail: Retrieve contact(s) with 
-    matching email & print to console or return error if none match
  ** 09 -TestContactList: demoContactList.retrieveZip()
- -- ContactList: METHOD retrieveZip: Retrieve contact(s) with 
-    matching zip & print to console or return error if none match
  ** 10 -TestContactList: demoContactList.writeFile()
  -- ContactList: METHOD writeFile: Write Contact object data from 
     ArrayList to file
-
 */
